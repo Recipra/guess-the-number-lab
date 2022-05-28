5fdcfdea9e9e2761a14cb9828a883f185369a2b2
recipra <jmo0425@outlook.com>
2022-05-28 14:28:42 -0400

    add prevGuesses and getGuess

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
   smallestNum: 1,
+  prevGuesses: [],
   secretNum: null,
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
+  },
+  getGuess: function() {
+    let guess
+    while (guess > this.biggestNum || guess < this.smallestNum) {
+      guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}!`)
+    }
+  },
 }
+game.getGuess()
+
+// console.log(game.getGuess())
